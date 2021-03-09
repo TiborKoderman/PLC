58a311dbf7887a05aa8e6345a31cb867b13ede51
tiborkoderman <tibor.koderman@gmail.com>
2021-03-09 23:41:19 +0100

    + made it so you can send and save data on the device config through bluetooth

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,44 @@
 #include "Adafruit_SH1106.h"
 #include "images.h"
 #include <cmath>
+#include "BluetoothSerial.h"
+#include <WiFi.h>
+#include <Preferences.h>
+#include <ArduinoJson.h>
+Preferences preferences;
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
 
-Adafruit_SH1106 display(21,22);
 
-bool toggle = false;
+Adafruit_SH1106 display(21,22); // Inicializira display
 
+BluetoothSerial SerialBT; // Inicializira bluetooth
+
+//Deklaracije funkcij
+//TODO: Prestavi v headerje
 void drawLoadingBar(int x0,int y0,int width,int height, int percentage);
 void bluetoothPairingMode();
 
+char WiFiSSID[32];
+char WiFiPassword[32];
+StaticJsonDocument<128> docWiFi;
 
 void setup() {
   Serial.begin(115200);
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+
+  preferences.begin("Settings", false);
+
+  //strcpy(WiFiSSID, preferences.getString("SSID","NULL");
+  preferences.getString("SSID","NULL").toCharArray(WiFiSSID, 32);
+  preferences.getString("Password","NULL").toCharArray(WiFiPassword,32);
+
+  Serial.println(String(WiFiSSID)+" "+String(WiFiPassword));
+  //strcpy(WiFiPassword, preferences.getString("Password","NULL");
 
   pinMode(BTNL, INPUT_PULLUP);
   pinMode(BTNR, INPUT_PULLUP);
@@ -29,7 +55,7 @@ void setup() {
   // pinMode(DCOUT2, OUTPUT);
   // pinMode(DCOUT3, OUTPUT);
   // pinMode(DCOUT4, OUTPUT);
-
+  
    pinMode(ACOUT1, OUTPUT);
   // pinMode(ACOUT2, OUTPUT);
   // pinMode(ACOUT3, OUTPUT);
@@ -37,41 +63,6 @@ void setup() {
 
   // ledcSetup(0, 5000,8);
   // ledcAttachPin(BUZZER, 0);
-  // //ledcWriteTone(0, 440);
-
-
-  // //WORKS
-  // digitalWrite(DCOUT1, HIGH);
-  // digitalWrite(DCOUT2, HIGH);
-  // digitalWrite(DCOUT3, HIGH);
-  // digitalWrite(DCOUT4, HIGH);
-  
-
-  // digitalWrite(ACOUT1, HIGH);
-  // digitalWrite(ACOUT2, HIGH);
-  // digitalWrite(ACOUT3, HIGH);
-  // digitalWrite(ACOUT4, HIGH);
-
-  // //ledcWriteTone(0, 440);
-  
-  // delay(1000);
-  // digitalWrite(DCOUT1, LOW);
-  // digitalWrite(DCOUT2, LOW);
-  // digitalWrite(DCOUT3, LOW);
-  // digitalWrite(DCOUT4, LOW);
-
-  // digitalWrite(ACOUT1, LOW);
-  // digitalWrite(ACOUT2, LOW);
-  // digitalWrite(ACOUT3, LOW);
-  // digitalWrite(ACOUT4, LOW);
-  // //ledcWriteTone(0, 0);
-  //display.drawBitmap(0, 0, Klogo, 128, 64, 1);
-  //display.drawBitmap(0, 0,  Klogo, 128, 64, 1);
- //display.display();
-
-
-
-  //display.drawCircle(display.width()/2, display.height()/2, 30, 1);
 
   bool flagBTpairMode = false;
 
@@ -89,9 +80,13 @@ void setup() {
       {
         buttonPressTimeout = millis();
         buttonsPressed = true;
+        Serial.println("both buttons pressed");
       }
       else if (millis()-buttonPressTimeout>2000 && buttonsPressed)
+      {
         flagBTpairMode = true;
+        Serial.println("entering bt mode...");
+      }
     }
     else
       buttonsPressed= false;
@@ -99,17 +94,14 @@ void setup() {
   if(flagBTpairMode)
     bluetoothPairingMode();
 
-  
+  display.clearDisplay();
+  display.drawBitmap(0, 0,  Klogo, 128, 64, 1);
+  display.display();
   delay(1000);
   display.clearDisplay();
-  //display.display();
-  //delay(10000);
-  
+  display.display();
 }
- 
 
- int counter =0;
- String  hw = "Test 2";
 void loop() {
 
 }
@@ -126,14 +118,67 @@ void drawLoadingBar(int x0,int y0,int width,int height, int percentage)
 
 void bluetoothPairingMode()
 {
+  Serial.println("Entering pairing mode");
+  long int timeout = millis();
+  bool on = true;
+   SerialBT.begin("ESP "+WiFi.macAddress());
    display.clearDisplay();
-  for(int i = 0; i<20; i++)
-  {
-    if(i%2==0)
+
+    while(!SerialBT.connected())
+    {
+      if(millis()-timeout > 500)
+      {
+        if(on)
+          display.drawBitmap(display.width()/2-21/2,display.height()/2-32/2,BTLogo,21,32,1);
+        else
+          display.clearDisplay();
+
+        display.display();
+        on = !on;
+        timeout = millis();
+      }
+    }
+
     display.drawBitmap(display.width()/2-21/2,display.height()/2-32/2,BTLogo,21,32,1);
-    else
-    display.clearDisplay();
-    display.display();
-    delay(500);
+        display.display();
+
+    while(SerialBT.connected())
+    {
+      Serial.println("Entered loop");
+
+          
+
+
+      if(SerialBT.available()){
+        String json = SerialBT.readString();
+        Serial.println(json);
+
+      DeserializationError err = deserializeJson(docWiFi,json);
+      if (err) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(err.f_str());
+        return;
+        }
+      //TODO: Check input logic, parse input
+
+      char ssid[32];
+      char pass[32];
+      Serial.println("Updating... ssid: "+String(ssid)+" password: "+String(pass));
+      strcpy(ssid,docWiFi["ssid"]);
+      strcpy(pass, docWiFi["pass"]);
+
+      
+      if(strlen(ssid)>0 && strlen(pass)>0)
+      {
+      preferences.putString("SSID", ssid);
+      preferences.putString("Password", pass);
+      Serial.println("Succesfully updated ssid: "+String(ssid)+" password: "+String(pass));
+      SerialBT.disconnect();
+      }
+      }
+
+ 
+    }
+    
   }
-}
+
